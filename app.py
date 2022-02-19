a038c8ddfd4bf8b6646f5b046a0a87a3ac7addf0
Pablo <nordiws@gmail.com>
2022-02-19 11:59:53 -0300

    fix: naming convention and adding comments

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 import cv2 as cv
-import image_preprocess as imgPre
+import utils.image_preprocess as imgPre
 import pytesseract
 from pytesseract import Output
 import numpy
-from pdf2img import pdf2img
+from utils.pdf2img import pdf2img
 import os
 from pathlib import Path
-from imageRotation import imgRotation
+from utils.image_rotation import image_rotation
 
 imgDir = 'img'
 
 # Converting PDF to image
 pdf2img()
 
-imgRotation()
+# Verify and rotate wrong orientations
+image_rotation()
 
+# Images array
 images = []
 
 for file in os.scandir(imgDir):
-    img = cv.imread(".\\"+imgDir+"\\"+file.name)
-    grayImg = imgPre.get_grayscale(img)
-    rawText = pytesseract.image_to_data(grayImg, output_type=Output.DICT)
-    txtOutput = numpy.asanyarray(rawText['text'])
-    filename = Path(file.name).stem
-    numpy.savetxt('.\\txt\\{}.txt'.format(filename), txtOutput, fmt='%s', newline=" ",)
+    img = cv.imread(".\\"+imgDir+"\\"+file.name)  # Read next image
+    grayscale_image = imgPre.get_grayscale(img)  # Apply grayscale preprocess
+    raw_output = pytesseract.image_to_data(grayscale_image, output_type=Output.DICT)  # Process image in Tesseract
+    text_output = numpy.asanyarray(raw_output['text'])  # Extract text from Tesseract output
+    filename = Path(file.name).stem  # Extract file name from image
+    # Save extracted text from image to txt file
+    numpy.savetxt('.\\txt\\{}.txt'.format(filename), text_output, fmt='%s', newline=" ",)
---
+++ utils/image_rotation.py
@@ -0,0 +1,18 @@
+import pytesseract
+from pytesseract import Output
+import os
+from PIL import Image
+
+
+def image_rotation():
+    image_dir = 'img'  # Defining folder name
+
+    # Iterating through each image in folder and extracting OSD information
+    for img in os.scandir(image_dir):
+        osd = pytesseract.image_to_osd(".\img\\" + img.name, output_type=Output.DICT)
+
+        # If the orientation is different from horizontal rotate the image
+        if osd['orientation'] != 0:
+            image = Image.open(".\img\\" + img.name)
+            rotated_image = image.rotate(osd['orientation'], expand=True)
+            rotated_image.save(".\img\\"+img.name)
