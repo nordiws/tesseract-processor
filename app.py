1f7c58f6886bb99dd0a40f58be57b9495f8e84f6
GONZALES Pablo (Consulting For ACCOR Hotels) <Pablo.GONZALES@consulting-for.accor.com>
2022-03-14 11:37:18 -0300

    Fixing imports and adding comments

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import cv2 as cv
-from cv2 import threshold
-import utils.image_preprocess as image_preprocess
 import pytesseract
 from pytesseract import Output
 import numpy
-from utils.pdf2img import pdf2img
 import os
 from pathlib import Path
+from utils.pdf2img import pdf2img
 from utils.image_rotation import image_rotation
+import utils.image_preprocess as image_preprocess
 
+# Images folder
 image_dir = 'img'
 
 # Converting PDF to image
@@ -24,12 +24,17 @@ for file in os.scandir(image_dir):
     image = cv.imread(".\\"+image_dir+"\\"+file.name)  # Read next image
 
     # Applying diferente preprocessing techniques to the current image
-    grayscale_image = image_preprocess.get_grayscale(image)  # Apply grayscale preprocess
+    grayscale_image = image_preprocess.get_grayscale(
+        image)  # Apply grayscale preprocess
 
     # Processing the image in Tesseract
-    raw_output = pytesseract.image_to_data(grayscale_image, output_type=Output.DICT)
-    text_output = numpy.asanyarray(raw_output['text'])  # Extract text from Tesseract output
-    filename = Path(file.name).stem  # Extract file name from image
+    raw_output = pytesseract.image_to_data(
+        grayscale_image, output_type=Output.DICT)
+
+    # Extract text from Tesseract output
+    text_output = numpy.asanyarray(raw_output['text'])
+    filename = Path(file.name).stem  # Extract file name from image file
 
     # Save extracted text from image to txt file
-    numpy.savetxt('.\\txt\\{}.txt'.format(filename), text_output, fmt='%s', newline=" ",)
+    numpy.savetxt('.\\txt\\{}.txt'.format(filename),
+                  text_output, fmt='%s', newline=" ",)
